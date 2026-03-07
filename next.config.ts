2029aafb6610a25a0cf5cc74755e251e786c0466
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-03-07 23:28:40 +0530

    Prepare for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
-  output: "export", // enable static export
-  basePath: "",     // empty for username.github.io root
-  assetPrefix: ".", // use relative paths for static files
 };
 
 export default nextConfig;
